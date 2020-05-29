12d4b0c565d085e1d926ec72d29ed3ac24c272e3
AliHasnain930 <alihascs830@gmail.com>
2020-05-28 18:49:05 -0700

    Currency Converter Web App

---
+++ src/currencyRow.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./styles.css";
+
+function currencyRow({
+  currencies,
+  defaultCurrency,
+  amount,
+  onChangeCurrency,
+  onChangeAmount,
+}) {
+  return (
+    <div className='currency-row'>
+      <input
+        type='number'
+        name='currencyInput'
+        id='currencyInput'
+        value={amount}
+        onChange={onChangeAmount}
+      />
+      <select
+        name='currencySelect'
+        id='currencySelect'
+        value={defaultCurrency}
+        onChange={onChangeCurrency}>
+        {currencies.map((currency) => {
+          return (
+            <option key={Math.random()} value={currency}>
+              {currency}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+}
+
+export default currencyRow;
